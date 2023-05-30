b65a122f2d33a6b43619d539d7af45a68955dd09
ciaranm23 <130502075+ciaranm23@users.noreply.github.com>
2023-05-30 13:12:16 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -57,3 +57,23 @@ def metrics (your_pred_act_list):
 
 
   return {'Precision': precision, 'Recall': recall, 'F1':f1, 'Accuracy':accuracy}
+
+def try_archs(full_table, target, architectures, thresholds):
+  train_table, test_table = up_train_test_split(full_table, target, .4)
+
+
+  for arch in architectures:
+    all_results=up_neural_net(train_table, test_table, arch, target)
+
+    all_mets = []
+    for t in thresholds:
+      all_predictions = [1 if pos>t else 0 for neg,pos in all_results]
+      pred_act_list = up_zip_lists(all_predictions, up_get_column(test_table, target))
+      mets = metrics(pred_act_list)
+      mets['Threshold'] = t
+      all_mets = all_mets + [mets]
+
+  print(f'Architecture: {arch}')
+  print(up_metrics_table(all_mets))
+
+  return None
